97caa105d569cc748120d10892ec2e1476d8be9b
natspindola <natspindola@hotmail.com>
2022-03-24 19:06:48 -0300

    criação BD

--- CadastroPessoa/Startup.cs
+++ CadastroPessoa/Startup.cs
@@ -1,6 +1,8 @@
+using CadastroPessoa.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,7 @@ namespace CadastroPessoa
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<Contexto>(opcoes => opcoes.UseSqlServer(Configuration.GetConnectionString("ConexaoBD")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
